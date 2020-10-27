3d6920a7bef3d07a2d267381d1c3b471dc178d86
unknown <jixuni@gmail.com>
2020-10-27 16:51:16 -0400

    updated menu items with navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import './App.css';
+import { Switch,Route } from 'react-router-dom'
 
 import HomePage from "./pages/homepage/homepage.component"
 
+const HatsPage = () => {
+  return (
+    <div>
+      Hats Page
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route  path='/hats' component={HatsPage} />
+      </Switch>
+      
     </div>
   );
 }
